bc78804b4e9e93ccfa9f4625738b99c02a4e3253
Illia Diachenko <ilya.dyachenko@gmail.com>
2024-10-12 17:27:29 +0300

    update pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Unbelievable-Notes/", // Replace <REPO_NAME> with your repository name
 });
