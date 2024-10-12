b8945f42e8374b32524000238178daac4e485ab0
Illia Diachenko <ilya.dyachenko@gmail.com>
2024-10-12 17:13:09 +0300

    Refactor safeBtoa and safeAtob functions in NotesApp

--- src/NotesApp.tsx
+++ src/NotesApp.tsx
@@ -2,6 +2,14 @@ import { ReactNode, useEffect, useState } from "react";
 import { useQueryParam } from "./useQueryParam";
 import "./NotesApp.css";
 
+function safeBtoa(input: string) {
+  return btoa(encodeURIComponent(input));
+}
+
+function safeAtob(input: string) {
+  return decodeURIComponent(atob(input));
+}
+
 function NotesApp(): ReactNode {
   const [note, setNote] = useState<string>("");
   const { getQueryParam, setQueryParam } = useQueryParam();
@@ -9,17 +17,13 @@ function NotesApp(): ReactNode {
   const handleNoteChange = (
     event: React.SyntheticEvent<HTMLDivElement>
   ): void => {
-    const newNote = event.currentTarget.textContent || "";
-    const encodedNote = btoa(newNote);
-    setQueryParam("n", encodeURIComponent(encodedNote));
+    const newNote = event.currentTarget.innerHTML || "";
+    setQueryParam("n", safeBtoa(newNote));
   };
 
   useEffect(() => {
     const note = getQueryParam("n");
-    const decodedNote = decodeURIComponent(note || "");
-    if (note) {
-      setNote(atob(decodedNote));
-    }
+    setNote(safeAtob(note || ""));
   }, [getQueryParam]);
 
   return (
